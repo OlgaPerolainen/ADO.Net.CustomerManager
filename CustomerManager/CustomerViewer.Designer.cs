d92b17147f3550e39e43176902cd9caa180ead35
OlgaPerolainen <95132545+OlgaPerolainen@users.noreply.github.com>
2022-02-25 20:55:21 +0300

    Add files via upload

---
+++ CustomerManager/CustomerViewer.Designer.cs
@@ -0,0 +1,337 @@
+﻿
+namespace CustomerManager
+{
+    partial class frmCustomerViewer
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.customerList = new System.Windows.Forms.ComboBox();
+            this.listBoxOrders = new System.Windows.Forms.ListBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblPhoto = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.textBoxAge = new System.Windows.Forms.TextBox();
+            this.btnChoosePhoto = new System.Windows.Forms.Button();
+            this.btnAddData = new System.Windows.Forms.Button();
+            this.btnShowData = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rBtnCustomers = new System.Windows.Forms.RadioButton();
+            this.rBtnOrders = new System.Windows.Forms.RadioButton();
+            this.lblId = new System.Windows.Forms.Label();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // customerList
+            // 
+            this.customerList.FormattingEnabled = true;
+            this.customerList.Location = new System.Drawing.Point(23, 27);
+            this.customerList.Name = "customerList";
+            this.customerList.Size = new System.Drawing.Size(243, 24);
+            this.customerList.TabIndex = 0;
+            // 
+            // listBoxOrders
+            // 
+            this.listBoxOrders.FormattingEnabled = true;
+            this.listBoxOrders.ItemHeight = 16;
+            this.listBoxOrders.Location = new System.Drawing.Point(23, 74);
+            this.listBoxOrders.Name = "listBoxOrders";
+            this.listBoxOrders.Size = new System.Drawing.Size(243, 148);
+            this.listBoxOrders.TabIndex = 1;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(284, 30);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 17);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "Имя";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(284, 67);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(70, 17);
+            this.lblLastName.TabIndex = 3;
+            this.lblLastName.Text = "Фамилия";
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(284, 104);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(49, 17);
+            this.lblAddress.TabIndex = 4;
+            this.lblAddress.Text = "Почта";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(284, 142);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(62, 17);
+            this.lblAge.TabIndex = 5;
+            this.lblAge.Text = "Возраст";
+            // 
+            // lblPhoto
+            // 
+            this.lblPhoto.AutoSize = true;
+            this.lblPhoto.Location = new System.Drawing.Point(284, 190);
+            this.lblPhoto.Name = "lblPhoto";
+            this.lblPhoto.Size = new System.Drawing.Size(44, 17);
+            this.lblPhoto.TabIndex = 6;
+            this.lblPhoto.Text = "Фото";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxName.Location = new System.Drawing.Point(370, 27);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(167, 22);
+            this.textBoxName.TabIndex = 7;
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxLastName.Location = new System.Drawing.Point(370, 64);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(167, 22);
+            this.textBoxLastName.TabIndex = 8;
+            // 
+            // textBoxAddress
+            // 
+            this.textBoxAddress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxAddress.Location = new System.Drawing.Point(370, 101);
+            this.textBoxAddress.Name = "textBoxAddress";
+            this.textBoxAddress.Size = new System.Drawing.Size(167, 22);
+            this.textBoxAddress.TabIndex = 9;
+            // 
+            // textBoxAge
+            // 
+            this.textBoxAge.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxAge.Location = new System.Drawing.Point(370, 139);
+            this.textBoxAge.Name = "textBoxAge";
+            this.textBoxAge.Size = new System.Drawing.Size(167, 22);
+            this.textBoxAge.TabIndex = 10;
+            // 
+            // btnChoosePhoto
+            // 
+            this.btnChoosePhoto.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnChoosePhoto.Location = new System.Drawing.Point(370, 183);
+            this.btnChoosePhoto.Name = "btnChoosePhoto";
+            this.btnChoosePhoto.Size = new System.Drawing.Size(167, 39);
+            this.btnChoosePhoto.TabIndex = 11;
+            this.btnChoosePhoto.Text = "Выберите файл...";
+            this.btnChoosePhoto.UseVisualStyleBackColor = true;
+            this.btnChoosePhoto.Click += new System.EventHandler(this.btnChoosePhoto_Click);
+            // 
+            // btnAddData
+            // 
+            this.btnAddData.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAddData.Location = new System.Drawing.Point(580, 27);
+            this.btnAddData.Name = "btnAddData";
+            this.btnAddData.Size = new System.Drawing.Size(123, 72);
+            this.btnAddData.TabIndex = 12;
+            this.btnAddData.Text = "Добавить данные";
+            this.btnAddData.UseVisualStyleBackColor = true;
+            this.btnAddData.Click += new System.EventHandler(this.btnAddData_Click);
+            // 
+            // btnShowData
+            // 
+            this.btnShowData.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnShowData.Location = new System.Drawing.Point(715, 27);
+            this.btnShowData.Name = "btnShowData";
+            this.btnShowData.Size = new System.Drawing.Size(123, 72);
+            this.btnShowData.TabIndex = 13;
+            this.btnShowData.Text = "Показать данные";
+            this.btnShowData.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnEdit.Location = new System.Drawing.Point(846, 27);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(123, 32);
+            this.btnEdit.TabIndex = 14;
+            this.btnEdit.Text = "Редактировать";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDelete.Location = new System.Drawing.Point(846, 67);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(123, 32);
+            this.btnDelete.TabIndex = 15;
+            this.btnDelete.Text = "Удалить";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.rBtnOrders);
+            this.groupBox1.Controls.Add(this.rBtnCustomers);
+            this.groupBox1.Location = new System.Drawing.Point(580, 116);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(389, 67);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Выбор данных";
+            // 
+            // rBtnCustomers
+            // 
+            this.rBtnCustomers.AutoSize = true;
+            this.rBtnCustomers.Location = new System.Drawing.Point(18, 30);
+            this.rBtnCustomers.Name = "rBtnCustomers";
+            this.rBtnCustomers.Size = new System.Drawing.Size(87, 21);
+            this.rBtnCustomers.TabIndex = 0;
+            this.rBtnCustomers.TabStop = true;
+            this.rBtnCustomers.Text = "Клиенты";
+            this.rBtnCustomers.UseVisualStyleBackColor = true;
+            // 
+            // rBtnOrders
+            // 
+            this.rBtnOrders.AutoSize = true;
+            this.rBtnOrders.Location = new System.Drawing.Point(240, 30);
+            this.rBtnOrders.Name = "rBtnOrders";
+            this.rBtnOrders.Size = new System.Drawing.Size(78, 21);
+            this.rBtnOrders.TabIndex = 1;
+            this.rBtnOrders.TabStop = true;
+            this.rBtnOrders.Text = "Заказы";
+            this.rBtnOrders.UseVisualStyleBackColor = true;
+            // 
+            // lblId
+            // 
+            this.lblId.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(577, 203);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(21, 17);
+            this.lblId.TabIndex = 17;
+            this.lblId.Text = "ID";
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxID.Location = new System.Drawing.Point(620, 200);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(349, 22);
+            this.textBoxID.TabIndex = 18;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(-1, 249);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(998, 289);
+            this.dataGridView1.TabIndex = 19;
+            // 
+            // frmCustomerViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(990, 529);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBoxID);
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnShowData);
+            this.Controls.Add(this.btnAddData);
+            this.Controls.Add(this.btnChoosePhoto);
+            this.Controls.Add(this.textBoxAge);
+            this.Controls.Add(this.textBoxAddress);
+            this.Controls.Add(this.textBoxLastName);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.lblPhoto);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.listBoxOrders);
+            this.Controls.Add(this.customerList);
+            this.MinimumSize = new System.Drawing.Size(1008, 576);
+            this.Name = "frmCustomerViewer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Customer Viewer";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox customerList;
+        private System.Windows.Forms.ListBox listBoxOrders;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label lblPhoto;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.TextBox textBoxAddress;
+        private System.Windows.Forms.TextBox textBoxAge;
+        private System.Windows.Forms.Button btnChoosePhoto;
+        private System.Windows.Forms.Button btnAddData;
+        private System.Windows.Forms.Button btnShowData;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rBtnOrders;
+        private System.Windows.Forms.RadioButton rBtnCustomers;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ CustomerManager/CustomerViewer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+using CodeFirst;
+
+namespace CustomerManager
+{
+    public partial class frmCustomerViewer : Form
+    {
+        SampleContext context = new SampleContext();
+        byte[] Ph;
+        public frmCustomerViewer()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddData_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Customer customer = new Customer
+                {
+                    Name = this.textBoxName.Text,
+                    LastName = this.textBoxLastName.Text,
+                    Email = this.textBoxAddress.Text,
+                    Age = Int32.Parse(this.textBoxAge.Text),
+                    Photo = Ph
+                };
+                context.Customers.Add(customer);
+                context.SaveChanges();
+                textBoxName.Text = String.Empty;
+                textBoxLastName.Text = String.Empty;
+                textBoxAddress.Text = String.Empty;
+                textBoxAge.Text = String.Empty;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message);
+            }
+        }
+
+        private void btnChoosePhoto_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                Image bm = new Bitmap(dialog.OpenFile());
+                ImageConverter converter = new ImageConverter();
+                Ph = (byte[])converter.ConvertTo(bm, typeof(byte[]));
+            }
+        }
+    }
+}
